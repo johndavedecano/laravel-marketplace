ad873959baeb78c9f5a912fe67b31eb5140b648c
John Dave Decano <johndavedecano@gmail.com>
2017-12-17 00:06:31 +0800

    Added user table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User([
+            'name' => 'John Dave Decano',
+            'email' => 'user@user.com',
+            'password' => '123456'
+        ]);
+
+        $user->save();
+    }
+}
