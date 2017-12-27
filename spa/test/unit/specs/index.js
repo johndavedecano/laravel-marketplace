831e92cbb50bf8fa942eae6007e3e9cf9ece55cc
John Dave Decano <johndavedecano@gmail.com>
2017-12-28 01:20:08 +0800

    Format spec index.js

--- spa/test/unit/specs/index.js
+++ spa/test/unit/specs/index.js
@@ -12,17 +12,21 @@ describe('Simple assertion test', function () {
     // assert that computed property `contextMessage` updates as intended
     return assert.equal(
       vm.contextMessage,
-      'foo Now go build something!', 'message doesnt equal foo')
+      'foo Now go build something!',
+      'message doesnt equal foo'
+    )
   })
 
   it('Make server request', function () {
     return fetch(TEST_API + '/ok')
-    .then(function (response) {
-      return response.text()
-    }).then(function (text) {
-      assert.equal(text, 'ok', 'response from server does not match')
-    }).catch(function (err) {
-      throw new Error(err.message)
-    })
+      .then(function (response) {
+        return response.text()
+      })
+      .then(function (text) {
+        assert.equal(text, 'ok', 'response from server does not match')
+      })
+      .catch(function (err) {
+        throw new Error(err.message)
+      })
   })
 })
