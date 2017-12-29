0a0cdba4d3011ef1888d060acb7252c6fc076c3b
John Dave Decano <johndavedecano@gmail.com>
2017-12-29 09:59:21 +0800

    fixed cors issue

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
