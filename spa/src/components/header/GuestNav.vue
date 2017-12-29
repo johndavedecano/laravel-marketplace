d82c9d7b3842b0d7ad8daab55f02834cbee55072
John Dave Decano <johndavedecano@gmail.com>
2017-12-29 14:46:10 +0800

    Added another nav item

--- spa/src/components/header/GuestNav.vue
+++ spa/src/components/header/GuestNav.vue
@@ -6,5 +6,8 @@
     <router-link class="nav-item" tag="li" to="/posts">
       <a class="nav-link">Posts</a>
     </router-link>
+    <router-link class="nav-item" tag="li" to="/about">
+      <a class="nav-link">About Us</a>
+    </router-link>
   </b-navbar-nav>
 </template>
