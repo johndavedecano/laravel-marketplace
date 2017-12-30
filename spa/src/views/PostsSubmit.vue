46527c9201f7c160077d85e111df0b9cb3b5816c
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:02:31 +0800

    Posts page submit

---
+++ spa/src/views/PostsSubmit.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  metaInfo: {
+    title: 'Submit Post',
+  },
+};
+</script>
+
+<template>
+  <page title="Submit Post">
+    <div class="row">
+      <div class="col-md-12">segasgeasg</div>
+    </div>
+  </page>
+</template>
