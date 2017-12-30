6d0cf4477046378efb792eebfa69b1cba95f6be7
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:02:12 +0800

    Renamed user posts page

---
+++ spa/src/views/PostsUserManager.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  metaInfo: {
+    title: 'Manage Posts',
+  },
+};
+</script>
+
+<template>
+  <page title="Manage Posts">
+    <div class="row">
+      <account-sidebar />
+    </div>
+  </page>
+</template>
