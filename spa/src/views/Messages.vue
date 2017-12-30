3742acdf6dd11fbc8a0375de37f8a2bf2c116f19
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:00:44 +0800

    Added messages page

---
+++ spa/src/views/Messages.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  metaInfo: {
+    title: 'Messages',
+  },
+};
+</script>
+
+<template>
+  <page title="Messages">
+    <div class="row">
+      <div class="col-md-12">segasgeasg</div>
+    </div>
+  </page>
+</template>
