6285e37afd8dbcf2d6bc5dc1e43b3f1ff67ad54e
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:00:33 +0800

    Added notifications page

---
+++ spa/src/views/Notifications.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  metaInfo: {
+    title: 'Notifications',
+  },
+};
+</script>
+
+<template>
+  <page title="Notifications">
+    <div class="row">
+      <div class="col-md-12">segasgeasg</div>
+    </div>
+  </page>
+</template>
