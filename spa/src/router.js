ac9f80d41e5203018a5bc4fdf3ad4869d1e84016
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:03:05 +0800

    Implemented new pages

--- spa/src/router.js
+++ spa/src/router.js
@@ -4,14 +4,18 @@ import AuthLayout from './layouts/AuthLayout'
 // Views goes here
 import About from './views/About.vue'
 import Account from './views/Account.vue'
+import Contact from './views/Contact.vue'
 import Forgot from './views/Forgot.vue'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Messages from './views/Messages.vue'
+import Notifications from './views/Notifications.vue'
 import NotFound from './views/NotFound.vue'
 import Posts from './views/Posts.vue'
+import PostsSubmit from './views/PostsSubmit.vue'
+import PostsUserManager from './views/PostsUserManager.vue'
 import Profile from './views/Profile.vue'
 import Register from './views/Register.vue'
-import UserPosts from './views/UserPosts.vue'
 // Todo use code splitting later.
 
 export default {
@@ -22,15 +26,31 @@ export default {
     { path: '/', component: Home },
     { path: '/posts', component: Posts },
     { path: '/about', component: About },
+    { path: '/contact', component: Contact },
     { path: '/account', component: Account, meta: { requireAuth: true } },
     {
       path: '/account/posts',
-      component: UserPosts,
+      component: PostsUserManager,
+      meta: { requireAuth: true }
+    },
+    {
+      path: '/account/posts/submit',
+      component: PostsSubmit,
       meta: { requireAuth: true }
     },
     {
       path: '/account/profile',
       component: Profile,
+      meta: { requireAuth: true }
+    },
+    {
+      path: '/account/messages',
+      component: Messages,
+      meta: { requireAuth: true }
+    },
+    {
+      path: '/account/notifications',
+      component: Notifications,
       meta: { requireAuth: true }
     },
     {
