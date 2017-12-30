33b6f2abe2614a74436b5e057871ec565b676e72
John Dave Decano <johndavedecano@gmail.com>
2017-12-30 16:59:24 +0800

    Added account navigation

---
+++ spa/src/components/AccountSidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="col-lg-3 mb-4">
+    <div class="list-group">
+      <router-link role="menuitem" class="list-group-item" to="/account" exact>Dashboard</router-link>
+      <router-link role="menuitem" class="list-group-item" to="/account/profile" exact>Account Settings</router-link>
+      <router-link role="menuitem" class="list-group-item" to="/account/posts" exact>Manage Posts</router-link>
+    </div>
+  </div>
+</template>
