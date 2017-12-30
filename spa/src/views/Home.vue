066c25b2f6d0f1cb48e1f322c192e21541e3bc2c
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:02:45 +0800

    Updated homepage with new layout

--- spa/src/views/Home.vue
+++ spa/src/views/Home.vue
@@ -1,10 +1,64 @@
+<script>
+import Carousel from './../components/Carousel.vue';
+
+export default {
+  components: {
+    Carousel,
+  },
+};
+</script>
+
 <template>
-  <div class="welcome container">
-    <h3>Welcome to vue build</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus nisl ut ipsum ultrices varius. Praesent mollis dolor et ex mattis mattis. Pellentesque non tortor lacinia, dignissim eros non, viverra purus. Aenean faucibus pellentesque facilisis. Aenean molestie nulla non libero viverra mattis. Nulla in consequat arcu. Vivamus egestas velit vestibulum metus ornare feugiat. Fusce semper libero euismod tellus pretium, at elementum metus gravida. Cras at fermentum metus. In vitae dui velit. Vivamus quis pharetra metus. Nullam nulla eros, vulputate ut vestibulum ut, lacinia non lectus. Vestibulum fermentum tristique sodales.</p>
-    <p>Ut eget bibendum augue. In hac habitasse platea dictumst. Maecenas euismod aliquam neque sed ultricies. Ut scelerisque, massa in cursus ultricies, justo arcu malesuada dui, id ornare dui est a libero. Maecenas fermentum at nibh a hendrerit. Nunc nunc risus, dignissim eget dui id, dignissim mattis turpis. Sed cursus vitae tellus vitae consectetur. Phasellus facilisis, odio eu euismod semper, odio erat pharetra purus, a cursus tortor odio ut dolor. Phasellus aliquam tristique iaculis. Phasellus placerat nibh vel elementum bibendum. Vestibulum eget ornare ligula. Etiam placerat enim est, at ullamcorper nibh tristique in. Integer velit sapien, blandit in nulla ac, pharetra vestibulum sem. Mauris efficitur convallis turpis et scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at tincidunt magna.</p>
-    <p>Donec scelerisque orci in dui fringilla, sed vulputate nisi placerat. Nullam viverra mi non tellus pretium tristique vehicula ut mi. Nam vel dui blandit ipsum viverra ultrices. Duis malesuada mi leo, sollicitudin ornare arcu pellentesque at. Aliquam vestibulum lacinia urna, et ornare libero ultricies nec. Praesent bibendum a justo in pretium. Aenean sollicitudin euismod lacus. Vestibulum suscipit ac sem ut ultricies. Sed lectus ex, pulvinar et mauris sit amet, fermentum molestie magna.</p>
-    <p>Morbi aliquet eget ipsum ac vulputate. Donec interdum cursus nibh vel fermentum. Nulla facilisi. Cras ac leo et justo sagittis tincidunt. Pellentesque finibus eros ut felis elementum, vitae malesuada augue elementum. Phasellus rhoncus ligula et ex maximus blandit sit amet vel orci. Duis vulputate eget odio vel dictum. Aliquam sapien ipsum, sodales vel auctor scelerisque, laoreet at augue. Pellentesque pulvinar, est eget vehicula semper, mi lorem imperdiet nisi, in vulputate lorem dui sed purus. Aenean eleifend luctus libero, vitae pellentesque dolor feugiat eu.</p>
-    <p>Aliquam rhoncus facilisis enim eu pellentesque. Cras faucibus magna fermentum, bibendum quam ut, mollis lectus. Morbi orci lorem, aliquet ut magna sit amet, auctor bibendum diam. Sed consequat ipsum fringilla risus luctus condimentum. In gravida, neque ut rhoncus lacinia, urna nulla elementum justo, eu feugiat lorem augue eu augue. Morbi semper vestibulum orci vel finibus. Vivamus non arcu eget turpis tempor ornare. Vivamus semper faucibus nisl ut luctus.</p>
+  <div>
+    <!-- HOMEPAGE SLIDER -->
+    <carousel />
+    <div class="container">
+      <h1 class="my-4">Welcome to the Marketplace</h1>
+      <!-- Marketing Icons Section -->
+      <div class="row home-section">
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Create An Account</h4>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Submit A Post</h4>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card h-100">
+            <h4 class="card-header">Get Connected</h4>
+            <div class="card-body">
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--- CALL TO ACTION -->
+      <div class="row mb-4">
+        <div class="col-md-8">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+        </div>
+        <div class="col-md-4">
+          <a class="btn btn-lg btn-secondary btn-block" href="#">Newsletter Signup</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
