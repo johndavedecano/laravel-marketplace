fd5cf82393e9f8b2f3155551dc38a29b530c111c
John Dave Decano <johndavedecano@gmail.com>
2017-12-31 06:01:38 +0800

    Used bootstrap vue carousel

---
+++ spa/src/components/Carousel.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <b-carousel id="carousel1"
+      style="text-shadow: 1px 1px 2px #333;"
+      controls
+      indicators
+      background="#ababab"
+      :interval="4000"
+      img-width="1024"
+      img-height="480"
+      v-model="slide"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="Best buy and sell platform"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://lorempixel.com/1024/480/technics/2/"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/4/">
+        <h1>Find Great Deals with Cheaper Prices</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/8/">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+        src="https://lorempixel.com/1024/480/technics/5/" alt="image slot">
+      </b-carousel-slide>
+    </b-carousel>
+  </header>
+</template>
+
+<style>
+.carousel-item {
+  height: 65vh;
+  min-height: 300px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+
