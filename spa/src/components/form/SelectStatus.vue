20d66230d5b75019a009621e7d842beedc4a24ba
John Dave Decano <johndavedecano@gmail.com>
2018-01-04 09:26:45 +0800

    Added status dropdown

---
+++ spa/src/components/form/SelectStatus.vue
@@ -0,0 +1,38 @@
+<template>
+<v-select
+  :value.sync="localValue"
+  :options="options"
+  placeholder="Select Status"
+>
+</v-select>
+</template>
+
+<script>
+import vSelect from 'vue-select';
+export default {
+  props: ['value'],
+  components: { vSelect },
+  data() {
+    return {
+      localValue: null,
+      options: [
+        {
+          value: 'active',
+          label: 'Active',
+        },
+        {
+          value: 'draft',
+          label: 'Draft',
+        },
+      ],
+    };
+  },
+  created() {
+    this.options.forEach(option => {
+      if (option.value === this.value) {
+        this.localValue = option;
+      }
+    });
+  },
+};
+</script>
